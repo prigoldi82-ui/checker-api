c126402642b188050ea722b4127cdea45eec95b1
prigoldi82-ui <prigoldi82@gmail.com>
2026-05-17 12:26:39 +0530

    bot.py

--- bot.py
+++ bot.py
@@ -10,16 +10,26 @@ def home():
 def checker():
 
     cc = request.args.get("cc")
+    url = request.args.get("url")
 
     if not cc:
         return jsonify({
-            "status": "error",
-            "message": "No CC provided"
+            "status": "Error",
+            "message": "No CC provided",
+            "error_code": "NO_CC"
+        })
+
+    if not url:
+        return jsonify({
+            "status": "Error",
+            "message": "Missing site param",
+            "error_code": "SITE DEAD"
         })
 
     return jsonify({
         "status": "LIVE",
         "cc": cc,
+        "site": url,
         "gateway": "Shopify",
         "message": "Checker working successfully"
     })
